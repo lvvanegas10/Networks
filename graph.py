9a1ba325cea3e7ac7237f9f0eb289d7b989fcd9b
Valeria <lauvalevanegas@gmail.com>
2018-09-27 19:41:34 -0500

    Graph

--- graph.py
+++ graph.py
@@ -1,4 +1,20 @@
 import pandas as pd
 import networkx as nx 
 
-print('yei')
+data = pd.ExcelFile('data.xlsx')
+G = nx.MultiDiGraph()
+i = 1
+print(data.sheet_names[i])
+x = data.parse(data.sheet_names[i])
+print(x)
+
+def addRoute(df):
+    for c in df.columns:
+        df[c].apply(addNodes)
+
+
+def addNodes(n):
+    print(n)
+
+
+## addRoute(x)
